39336c6efa1424c5bb1ef973ffd9a592a0083dea
Ana0251 <86150908+Ana0251@users.noreply.github.com>
2022-11-20 17:46:19 +0330

    Update settings.py

--- notebook/settings.py
+++ notebook/settings.py
@@ -82,8 +82,10 @@ WSGI_APPLICATION = 'notebook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'notebook_620w',
+        'USER': 'ana0251'
+        'PASSWORD': 'WoNjtAd7rb82TupMGJSKTN9jAMp9XVVI'
     }
 }
 
